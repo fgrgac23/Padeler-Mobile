b6a8c6355d33ca450c6aee97aad70c2e079eaccb
Filip Grgac <fgrgac23@student.foi.hr>
2026-04-01 00:51:52 +0200

    Add files via upload

---
+++ Software/RAMPU2506_padeler/app/src/test/java/com/example/rampu2506_padeler/ExampleUnitTest.kt
@@ -0,0 +1,12 @@
+package com.example.rampu2506_padeler
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
